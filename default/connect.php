fee788464a0f86d27f538f00b81ead29efe00f92
Krish-Sah <164937863+Krish-Sah@users.noreply.github.com>
2024-05-21 16:01:42 +0400

    Update connect.php

--- default/connect.php
+++ default/connect.php
@@ -74,12 +74,12 @@ mysqli_close($con);
     </div>
 
     <div class="main">
-        <seection class="section">
+        <section class="section">
             <div class="container">
                 <div id="margin_zero" class="content has-text-centered is-size-6 clr-primary">Please wait while you are being</div>
                 <div id="margin_zero" class="content has-text-centered is-size-6 clr-primary">authorized on the network</div>
             </div>
-        </seection>
+        </section>
     </div>
 
 </div>
